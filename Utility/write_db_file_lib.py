5ca46822f0e1ea44851c4ede4665df80b969f884
Sreenivasulu <novemberautomationbatch@gmail.com>
2024-02-10 08:38:15 +0530

    Master template added

--- Utility/write_db_file_lib.py
+++ Utility/write_db_file_lib.py
@@ -17,6 +17,11 @@ def write_output(TC_ID,Test_Case_Name,Number_of_source_Records,
 
 #This is pull request verification
 
-def write_output2(TC_ID,Test_Case_Name,Number_of_source_Records,
+def write_output2(TC_ID,TC_ID2,Test_Case_Name,Number_of_source_Records,
+                 Number_of_target_Records,Status,Number_of_failed_Records,Out):
+    Out["TC_ID"].append(TC_ID)
+    Out["TC_ID2"].append(TC_ID2)
+
+def write_output3(TC_ID,Test_Case_Name,Number_of_source_Records,
                  Number_of_target_Records,Status,Number_of_failed_Records,Out):
     Out["TC_ID"].append(TC_ID)
