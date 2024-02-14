f967ae911cfd60ffddbf31601d9dcf9716fbdecc
Sreenivasulu <novemberautomationbatch@gmail.com>
2024-02-14 07:03:28 +0530

    Master template added

--- Utility/files_read_lib.py
+++ Utility/files_read_lib.py
@@ -1,8 +1,8 @@
 import logging
-import os
 import json
+import os
 
-import  pkg_resources
+import pkg_resources
 
 from pyspark.sql.types import StructType
 
@@ -13,23 +13,24 @@ logging.basicConfig(filename="newfile.log",
 logger = logging.getLogger()
 
 
-def read_data(format, path, spark, delimiter=None, multiline=None, sql_path=None, database=None, schema=None):
+def read_data(format, path, spark, multiline="NA", sql_path=None, database=None, schema=None):
     print(schema)
     if format.lower() == 'csv':
-        if schema is None:
+        if schema == 'NA':
             df = spark.read.option("header", True).option("delimiter", ",").csv(path)
             logger.info("CSV file has read successfully from the below path" + path)
         else:
             print(type(schema))
-            schema = pkg_resources.resource_filename("schema", schema)
-            with open(schema, 'r') as schema_file:
+            #schema = pkg_resources.resource_filename("schema", schema)
+            schema = os.environ.get("project_path")
+            with open(schema+'/schema/contact_info_schema.json', 'r') as schema_file:
                 schema = StructType.fromJson(json.load(schema_file))
                 print(schema)
             df = spark.read.schema(schema).option("header", True).option("delimiter", ",").csv(path)
             logger.info("CSV file has read successfully from the below path" + path)
 
     elif format.lower() == 'json':
-        if multiline is None:
+        if multiline == 'NA':
             df = spark.read.json(path)
             logger.info("Json file has read successfully from the below path" + path)
 
@@ -49,18 +50,26 @@ def read_data(format, path, spark, delimiter=None, multiline=None, sql_path=None
         conf_file_path = pkg_resources.resource_filename('Config', 'config.json')
         with open(conf_file_path, 'r') as f:
             config_data = json.loads(f.read())[database]
-        if sql_path is not None:
+        if sql_path != "NA":
             sql_path = pkg_resources.resource_filename('Transformations_queries', sql_path)
             with open(sql_path, "r") as file:
                 sql_query = file.read()
-        print(sql_query)
-        print(config_data)
-        df = spark.read.format("jdbc"). \
-            option("url", config_data['url']). \
-            option("user", config_data['user']). \
-            option("password", config_data['password']). \
-            option("query", sql_query). \
-            option("driver", config_data['driver']).load()
+            print(sql_query)
+            print(config_data)
+            df = spark.read.format("jdbc"). \
+                option("url", config_data['url']). \
+                option("user", config_data['user']). \
+                option("password", config_data['password']). \
+                option("query", sql_query). \
+                option("driver", config_data['driver']).load()
+        elif sql_path == 'NA':
+            df = spark.read.format("jdbc"). \
+                option("url", config_data['url']). \
+                option("user", config_data['user']). \
+                option("password", config_data['password']). \
+                option("dbtable", path). \
+                option("driver", config_data['driver']).load()
+
     else:
         logger.critical("File format is not found ")
     return df
--- main.py
+++ main.py
@@ -1,102 +1,104 @@
+"""This file will be starting point for automation execution"""
 
-
+from Utility.files_read_lib import *
 from Utility.validation_lib import *
 import pandas as pd
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import collect_set
 
-jar_path = pkg_resources.resource_filename('jars', 'postgresql-42.2.5.jar')
-#jar_path = "/Users/harish/PycharmProjects/Data_Automation_project/jars/ojdbc11.jar"
-spark = SparkSession.builder.master("local")\
+os.environ.setdefault("project_path", os.getcwd())
+project_path = os.environ.get("project_path")
+
+# jar_path = pkg_resources.resource_filename('jars', 'postgresql-42.2.5.jar')
+jar_path = project_path + "/jars/postgresql-42.2.5.jar"
+print(jar_path)
+spark = SparkSession.builder.master("local") \
     .appName("test") \
-    .config("spark.jars",jar_path)\
-    .config("spark.driver.extraClassPath",jar_path) \
-    .config("spark.executor.extraClassPath",jar_path) \
+    .config("spark.jars", jar_path) \
+    .config("spark.driver.extraClassPath", jar_path) \
+    .config("spark.executor.extraClassPath", jar_path) \
     .getOrCreate()
-template_path = pkg_resources.resource_filename("Config","Master_Test_Template.xlsx")
-#template_path = "/Users/harish/PycharmProjects/Data_Automation_project/Config/Master_Test_Template.xlsx"
+# template_path = pkg_resources.resource_filename("Config", "Master_Test_Template.xlsx")
+template_path = project_path + '/Config/Master_Test_Template.xlsx'
+print(template_path)
 Test_cases = pd.read_excel(template_path)
-run_test_case = Test_cases.loc[(Test_cases.execution_ind=='Y')]
-print(run_test_case)
+run_test_case = Test_cases.loc[(Test_cases.execution_ind == 'Y')]
+# print(run_test_case)
 # print(run_test_case.columns)
-# df = spark.createDataFrame(run_test_case)
+df = spark.createDataFrame(run_test_case)
+
+validations = df.groupBy('source', 'source_type',
+                         'source_db_name', 'schema_path', 'source_transformation_query_path', 'target',
+                         'target_type', 'target_db_name', 'target_transformation_query_path',
+                         'key_col_list', 'null_col_list', 'unique_col_list').agg(
+    collect_set('validation_Type').alias('validation_Type'))
+
+validations.show(truncate=False)
 #
-# validations = df.groupBy('source', 'source_type',
-#        'source_db_name','schema_path', 'source_transformation_query_path', 'target',
-#        'target_type', 'target_db_name', 'target_transformation_query_path',
-#        'key_col_list', 'null_col_list', 'unique_col_list').agg(collect_set('validation_Type').alias('validation_Type'))
-#
-# validations.show(truncate=False)
-# #
-# validations = validations.collect()
-#
-#
-# Out = {"TC_ID":[],
-#        "test_Case_Name":[],
-#        "Source_name":[],
-#        "target_name":[],
-#        "Number_of_source_Records":[],
-#        "Number_of_target_Records":[],
-#        "Number_of_failed_Records":[],
-#        "column":[],
-#        "Status":[],
-#        }
-# schema= ["TC_ID",
-#          "test_Case_Name",
-#          "Source_name",
-#          "target_name",
-#          "Number_of_source_Records",
-#          "Number_of_target_Records",
-#          "Number_of_failed_Records",
-#          "column",
-#          "Status"]
-#
-#
-#
-# for row in validations:
-#     if row['source_type'] == 'table':
-#         source = read_data(row['source_type'], row['source'], spark=spark, database=row['source_db_name'],sql_path=row['source_transformation_query_path'])
-#     else:
-#         source_path = pkg_resources.resource_filename('source_files', row['source'])
-#         print("Source_path",source_path)
-#         source = read_data(row['source_type'], source_path, spark,schema=row['schema_path'])
-#
-#     if row['target_type'] == 'table':
-#         print(row['target_type'], row['target'], row['target_db_name'])
-#         target = read_data(row['target_type'], row['target'], spark=spark, database=row['target_db_name'],sql_path=row['target_transformation_query_path'])
-#     else:
-#         target_path = pkg_resources.resource_filename('source_files', row['target'])
-#         print(source_path)
-#         target = read_data(row['target_type'], target_path, spark)
-#
-#     source.show(n=2)
-#     target.show(n=2)
-#     for validation in row['validation_Type']:
-#         print(validation)
-#         if validation == 'count_validation':
-#             count_validation(source, target, Out,row)
-#         elif validation == 'duplicate':
-#             duplicate(target,row['key_col_list'], Out,row)
-#         elif validation == 'Null_value_check':
-#             Null_value_check(target, row['null_col_list'], Out,row)
-#         elif validation == 'Uniquess_check':
-#             Uniquess_check(target, row['unique_col_list'], Out,row)
-#
-#         elif validation == 'records_present_only_in_source':
-#             records_present_only_in_source(source, target, row['key_col_list'], Out,row)
-#
-#         elif validation == 'records_present_only_in_target':
-#             records_present_only_in_target(source, target, row['key_col_list'], Out,row)
-#
-#         elif validation == 'data_compare':
-#             data_compare(source, target, row['key_col_list'], Out)
-#
-#
-#
-# df = pd.DataFrame(Out)
-#
-# df.to_csv("summary.csv")
-#
-#
-# spark.createDataFrame(df).show()
+validations = validations.collect()
+
+Out = {"TC_ID": [],
+       "test_Case_Name": [],
+       "Source_name": [],
+       "target_name": [],
+       "Number_of_source_Records": [],
+       "Number_of_target_Records": [],
+       "Number_of_failed_Records": [],
+       "column": [],
+       "Status": [],
+       }
+schema = ["TC_ID",
+          "test_Case_Name",
+          "Source_name",
+          "target_name",
+          "Number_of_source_Records",
+          "Number_of_target_Records",
+          "Number_of_failed_Records",
+          "column",
+          "Status"]
+
+for row in validations:
+    print("*" * 50)
+    print(row)
+    print("Execution started for dataset ".center(50))
+    print("*" * 50)
+
+    if row['source_type'] == 'table':
+        source = read_data(row['source_type'], row['source'], spark=spark, database=row['source_db_name'],
+                               sql_path=row['source_transformation_query_path'])
+    else:
+        source_path = pkg_resources.resource_filename('source_files', row['source'])
+        print("Source_path", source_path)
+        source = read_data(row['source_type'], source_path, spark, schema=row['schema_path'])
+
+    if row['target_type'] == 'table':
+        print(row['target_type'], row['target'], row['target_db_name'])
+        target = read_data(row['target_type'], row['target'], spark=spark, database=row['target_db_name'],
+                               sql_path=row['target_transformation_query_path'])
+    else:
+        target_path = pkg_resources.resource_filename('source_files', row['target'])
+        target = read_data(row['target_type'], target_path, spark)
+
+    source.show(n=2)
+    target.show(n=2)
+    for validation in row['validation_Type']:
+
+        print(validation)
+        if validation.strip().lower() == 'count_check':
+            count_check(source, target, Out, row)
+        elif validation == 'duplicate_check':
+            duplicate_check(target, row['key_col_list'], Out, row)
+        elif validation == 'null_value_check':
+            null_value_check(target, row['null_col_list'], Out, row)
+        elif validation == 'uniqueness_check':
+            uniqueness_check(target, row['unique_col_list'], Out, row)
+        elif validation == 'records_present_only_in_source':
+            records_present_only_in_source(source, target, row['key_col_list'], Out, row)
+        elif validation == 'records_present_only_in_target':
+            records_present_only_in_target(source, target, row['key_col_list'], Out, row)
+        elif validation == 'data_compare':
+            data_compare(source, target, row['key_col_list'], Out,row)
+
+df = pd.DataFrame(Out)
+df.to_csv("summary.csv")
+spark.createDataFrame(df).show()
